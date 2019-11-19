26d1535b349b25ad792aa62d63d65d3a044cebb2
Дмитрий Конечный <tequila2908@iMac-Dmitrij.local>
2019-11-19 22:44:32 +0300

    Домашнее задание к занятию 1.1 «Базовый синтаксис»

---
+++ variables/main.js
@@ -0,0 +1,23 @@
+//Задание 1
+
+let algebra = 3;
+let geography = 3;
+let physics = 2;
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+// Задание 2
+
+let myName = 'Tequila2908';
+let message = `Привет, мир! Меня зовут ${myName}.`;
+console.log(message);
+
+// Задание 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+
