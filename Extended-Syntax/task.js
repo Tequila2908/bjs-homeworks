3dd8dc4dae82b3eadf238889664fc32754a3e5f9
macbook air <macbookair@MacBook-Air-macbook.local>
2019-11-21 20:12:32 +0300

    Домашнее задание к занятию 1.2 «Расширенный синтаксис»

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -13,7 +13,17 @@ function calculateQuadraticEquation(){
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
     //return x;
+    let d = b * b - 4 * a * c,
+    square = Math.sqrt(d);
+    if (d < 0) {
+    	return [ null ];
+    } else if (d = 0) {
+    	return [ (-b / 2 * a) ];
+    } else {
+    	return [ ((-b + square) / 2 * a), ((-b - square) / 2 * a) ];
+    }
 }
+
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
@@ -24,7 +34,23 @@ function calculateAverageRating(){
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
     //return averageMark;
+  let sum = 0;
+  if (marks.length <= 5) {
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];
+    }
+    return sum / marks.length;
+  } else if (marks.length > 5) {
+      marks = marks.slice(0, 5);
+      for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+      }
+      return sum / marks.length;
+  }
+
 }
+
+getAverageMark(marks);
 
 function calculateDrinkTask(){
     let name = window.personName.value;
@@ -35,6 +61,20 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+
+  let year = (new Date).getFullYear();
+  let age = year - dateOfBirthday.getFullYear();
+  console.log(age);
+  console.log(year);
+
+
+  if (age < 18) {
+    return(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+  }
+  else { 
+    return(`Не желаете ли олд-фэшн, ${name}?`);
+  }
 }
+
+
+
