3538ed581e9d610c9bf154487a2c06de7f5b072a
macbook air <macbookair@MacBook-Air-macbook.local>
2019-12-29 12:20:41 +0300

    Правки

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -8,41 +8,39 @@ const sum = (a, b) => a + b;
 function memoize(fn, limit) {
   const results = [];
   
-  return function(a, b) {
-    if (results[0] === undefined) {
+  return function(a, b, ...args) {
+    for (let arg of args) {
+      b += arg; 
+    }
+    let test10 = results.find(index => compareArrays(index.args, [a, b]));
+    if (test10) {
+      console.log('results');
+      return results[results.indexOf(test10)].result    
+    } else {
+      if (results.length ==  limit) {
+        results.shift();
+      }
+      if (!test10) {
       let res = {
           args: [a, b],
           result: sum (a, b)
         }
       results.push(res);
       return res.result;
-    } else {
-        if (results.find(index => compareArrays(index.args, [a, b]))) {
-          console.log('test');
-          return results[results.indexOf(results.find(index => compareArrays(index.args, [a, b])))].result
-          
-        } else {
-          if (results.length === limit) {
-            results.shift();
-          }
-          let res = {
-          args: [a, b],
-          result: sum (a, b)
-          }
-          results.push(res);
-          return res.result;
-          }
+      }
     }
+
   }
 }
 
 const mSum = memoize(sum, 2);
+mSum(1, 2, 3, 4);
+mSum(1, 2, 4, 3);
 mSum(1, 2);
-mSum(1, 2);
-mSum(2, 4);
+//mSum(2, 4);
 //mSum(1, 2);
-mSum(3, 5);
-mSum(3, 5);
+//mSum(3, 5);
+//mSum(3, 5);
 
 
 
